02922d5e5e0692e957214513932cf2d40dea7f0f
Katya Zhukova <gjugjuka2002@gmail.com>
2023-09-22 22:17:11 +0400

    Fix tests
    Ранее в тестах создавалось графическое окно, что приводило к ошибке тестирования в среде, где отсутствует графический дисплей. Поэтому, чтобы избежать ошибки, сейчас используются заглушки.

--- test.py
+++ test.py
@@ -1,72 +1,71 @@
 import unittest
-import tkinter as tk
+from unittest.mock import Mock
 from main import validate_input
 
 class TestValidateInput(unittest.TestCase):
 
-    def setUp(self):
-        # Создаем окно Tkinter для тестирования
-        self.root = tk.Tk()
-
-    def tearDown(self):
-        # Уничтожаем окно Tkinter после каждого теста
-        self.root.destroy()
-
     def test_valid_positive_integer1(self):
         # Тестирование ввода допустимого положительного целого числа
         input_num = "50"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertTrue(result)
-        self.assertEqual(error_label.cget("text"), "")  # Проверка, что текст метки пуст
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="")
 
     def test_valid_positive_integer2(self):
         # Тестирование ввода допустимого положительного целого числа
         input_num = "10"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertTrue(result)
-        self.assertEqual(error_label.cget("text"), "")  # Проверка, что текст метки пуст
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="")
 
     def test_negative_integer1(self):
         # Тестирование ввода отрицательного целого числа
         input_num = "-1337"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertFalse(result)
-        self.assertEqual(error_label.cget("text"), "Enter a positive integer greater than 0")
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="Введите целое положительное число больше 0")
 
     def test_negative_integer2(self):
         # Тестирование ввода отрицательного целого числа
-        input_num = "-228"
-        error_label = tk.Label()
+        input_num = "-1488"
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertFalse(result)
-        self.assertEqual(error_label.cget("text"), "Enter a positive integer greater than 0")
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="Введите целое положительное число больше 0")
 
     def test_zero_input(self):
         # Тестирование ввода нуля
         input_num = "0"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertFalse(result)
-        self.assertEqual(error_label.cget("text"), "Enter a positive integer greater than 0")
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="Введите целое положительное число больше 0")
 
-    def test_non_numeric_input1(self):
+    def test_letters_input(self):
         # Тестирование ввода нечисловых данных
         input_num = "abcdefg"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertFalse(result)
-        self.assertEqual(error_label.cget("text"), "Enter a numeric value")
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="Введите числовое значение")
 
-    def test_non_numeric_input2(self):
+    def test_symbols_input(self):
         # Тестирование ввода нечисловых данных
         input_num = ";*"
-        error_label = tk.Label()
+        error_label = Mock()  # Создаем заглушку для tk.Label()
         result = validate_input(input_num, error_label)
         self.assertFalse(result)
-        self.assertEqual(error_label.cget("text"), "Enter a numeric value")
+        # Проверка, что заглушка была вызвана с нужными аргументами
+        error_label.config.assert_called_once_with(text="Введите числовое значение")
 
 if __name__ == '__main__':
     unittest.main()
